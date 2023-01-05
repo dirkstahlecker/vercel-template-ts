e114de9a91ac23473c3640975e0206801c11f27e
Dirk Stahlecker <dirkstahlecker24@gmail.com>
2023-01-04 20:32:15 -0700

    added in styles

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,7 @@
 import '../style/index.css'
+import '../style/JournalReader.css';
+import '../style/JournalWriter.css';
+import '../style/MainApp.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />

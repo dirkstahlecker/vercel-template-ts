4409507c39d7b8f15c200c626447bf5be593d252
Dirk Stahlecker <dirkstahlecker24@gmail.com>
2023-07-12 20:14:49 -0400

    pared down somewhat

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App box">
-      <h1>Dirk Stahlecker Racing</h1>
-      <h2>#47 Micro Sprint Racer from Cambridge, MA</h2>
-      <a href="mailto:dirkstahleckerracing@gmail.com">dirkstahleckerracing@gmail.com</a>
-      <br/>
-      <br/>
-      <a href="https://www.facebook.com/dirkstahleckerracing/">See updates on Facebook</a>
-      <br/>
-      <br/>
-      <p class="footer-note">This site is a work in progress, more to come soon!</p>
+      <a href="/recaps/5-19-23_Clinton_County_Speedway.pdf" target="_blank">5-19-23: Clinton County Speedway</a>
     </div>
   );
 }

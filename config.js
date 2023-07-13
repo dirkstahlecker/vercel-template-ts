647808b8ce3f4171ed2fda274a735532b0201be6
Dirk Stahlecker <dirkstahlecker24@gmail.com>
2023-07-12 20:11:01 -0400

    just copied from dirk-stahlecker-racing

--- config.js
+++
@@ -1,8 +0,0 @@
-// const config = {
-//   connectionString: process.env.DATABASE_URL || "postgresql://dirk:pE_TGdnzCQX2nsQC9JYnhw@aqua-wyvern-4580.6wr.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
-//   ssl: {
-//     rejectUnauthorized: true,
-//     ca: process.env.CERT
-//   }
-// };
-// exports.config = config;
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App box">
+      <h1>Dirk Stahlecker Racing</h1>
+      <h2>#47 Micro Sprint Racer from Cambridge, MA</h2>
+      <a href="mailto:dirkstahleckerracing@gmail.com">dirkstahleckerracing@gmail.com</a>
+      <br/>
+      <br/>
+      <a href="https://www.facebook.com/dirkstahleckerracing/">See updates on Facebook</a>
+      <br/>
+      <br/>
+      <p class="footer-note">This site is a work in progress, more to come soon!</p>
+    </div>
+  );
+}
+
+export default App;
